160232021b671848aca3553d3ba37a3cf911d15f
Krzysztof Siuda <kksiuda@gmail.com>
2023-11-17 12:08:09 +0000

    add exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 export type { LogConsumer, LogFormatter, LogLevel } from './types';
 
 export { Log } from './Log';
+
 export { consoleLogConsumerFactory } from './consumers/console';
 export { textFileLogConsumerFactory } from './consumers/text-file';
 export { sqliteLogConsumerFactory } from './consumers/sqlite';
+
+export { passthroughLogFormatter } from './formatters/passthroughLogFormatter';
+export { stringLogFormatterFactory } from './formatters/stringLogFormmater';
 export { nullLog } from './instances';
